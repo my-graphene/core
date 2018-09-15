35f5ac9fb0899d6ff31f4de66d36a174deeb871a
Li RuiGang <yueyemingming@163.com>
2018-09-15 16:01:20 +0800

    delete graphenelib

--- contracts/graphenelib/graphene.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <graphenelib/types.hpp>
-#include <graphenelib/action.hpp>
-#include <graphenelib/print.hpp>
-#include <graphenelib/dispatcher.hpp>
-#include <graphenelib/contract.hpp>
