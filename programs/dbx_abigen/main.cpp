ce617c40643f5dc915c45375567d319ba32d672c
Li RuiGang <yueyemingming@163.com>
2018-09-25 17:24:34 +0800

    contract

--- programs/dbx_abigen/main.cpp
+++ programs/dbx_abigen/main.cpp
@@ -58,7 +58,7 @@ std::unique_ptr<FrontendActionFactory> create_find_macro_factory(string &contrac
 
         clang::FrontendAction *create() override
         {
-            return new find_gxc_abi_macro_action(contract, actions, abi_context);
+            return new find_dbx_abi_macro_action(contract, actions, abi_context);
         }
     };
 
